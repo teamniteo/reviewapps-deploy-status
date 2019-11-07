11fb106380cf4e244e3077c840379386a6c89ef3
Arijit Basu <sayanarijit@gmail.com>
2019-11-07 16:58:46 +0530

    Add some options for more control
    - Option to choose the checks that needs to be performed.
    - Option to define a delay for the build and app load stages.
    - Plus some code improvements and docs update.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.7-alpine
 RUN pip install --upgrade pip
 RUN pip install requests==2.22.0
 
-COPY review_app_status.py /
+WORKDIR /app
+COPY . .
 
-ENTRYPOINT ["python3", "/review_app_status.py"]
+CMD ["python3", "/app/review_app_status.py"]
--- review_app_status.py
+++ review_app_status.py
@@ -4,7 +4,9 @@ import json
 import logging
 import os
 import time
-from enum import Enum
+import typing as t
+from dataclasses import dataclass
+from enum import Enum, auto
 
 import requests
 
@@ -12,10 +14,46 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("review_app_status")
 
 
-SUCCESS = "success"  # Review App Success Build state
+class BuildStates(Enum):
+    """Expected reviewapp app build states"""
+
+    success = "success"
 
 
-def _make_github_api_request(url):
+class Checks(Enum):
+    """Available checks"""
+
+    # Check if the build was success
+    build = auto()
+
+    # Check the HTTP response from app URL
+    response = auto()
+
+
+@dataclass(frozen=True)
+class Args:
+    """User input arguments"""
+
+    # Checks to be performed
+    checks: t.List[Checks]
+
+    # Delay for the application to be built in Heroku
+    build_time_delay: int
+
+    # Delay for the application to load and start serving
+    load_time_delay: int
+
+    # Interval between the repeating checks
+    interval: int
+
+    # Acceptable responses for the response check
+    accepted_responses: t.List[int]
+
+    # Max time spend retrying for the build check.
+    deployments_timeout: t.List[int]
+
+
+def _make_github_api_request(url: str) -> dict:
     """Make github API request with `deployment` event specific headers.
 
     Input:
@@ -33,7 +71,9 @@ def _make_github_api_request(url):
     return r.json()
 
 
-def _get_github_deployment_status_url(deployments_url, commit_sha, timeout, interval):
+def _get_github_deployment_status_url(
+    deployments_url: str, commit_sha: str, timeout: int, interval: int
+) -> None:
     """Get deployment_status URL for the head commit.
         Inputs:
             deployments_url: This can be obtained from `pull_request` event payload.
@@ -60,7 +100,7 @@ def _get_github_deployment_status_url(deployments_url, commit_sha, timeout, inte
     raise ValueError("No deployment found for the lastest commit.")
 
 
-def _get_build_data(url, interval):
+def _get_build_data(url: str, interval: int) -> dict:
     """Get Review App build data using Github's `deployment_status` API.
     
         Inputs:
@@ -92,7 +132,9 @@ def _get_build_data(url, interval):
         return response[0]
 
 
-def _check_review_app_deployment_status(review_app_url, accepted_responses):
+def _check_review_app_deployment_status(
+    review_app_url: str, accepted_responses: t.List[int]
+):
     """Check Review App deployment status code against accepted_responses.
     
     Inputs:
@@ -107,38 +149,63 @@ def _check_review_app_deployment_status(review_app_url, accepted_responses):
         r.raise_for_status()
 
 
-def main():
+def main() -> None:
     """Main workflow.
     
     All the inputs are received from workflow as environment variables.
     """
-    interval_arg = int(os.environ["INPUT_INTERVAL"])
-    deployments_timeout_arg = int(os.environ["INPUT_DEPLOYMENTS_TIMEOUT"])
-    accepted_responses_arg = os.environ["INPUT_ACCEPTED_RESPONSES"]
-    event_payload_path = os.environ["GITHUB_EVENT_PATH"]
 
-    logger.info(f"Statuses being accepted: {accepted_responses_arg}")
-    accepted_responses = set(map(int, accepted_responses_arg.split(",")))
-
-    with open(event_payload_path) as f:
-        data = f.read()
-    pull_request_data = json.loads(data)
-
-    github_deployment_status_url = _get_github_deployment_status_url(
-        pull_request_data["repository"]["deployments_url"],
-        pull_request_data["pull_request"]["head"]["sha"],
-        deployments_timeout_arg,
-        interval_arg,
+    args = Args(
+        checks=[Checks[x.strip()] for x in os.environ["INPUT_CHECKS"].split(",")],
+        build_time_delay=int(os.environ["INPUT_BUILD_TIME_DELAY"]),
+        load_time_delay=int(os.environ["INPUT_LOAD_TIME_DELAY"]),
+        interval=int(os.environ["INPUT_INTERVAL"]),
+        deployments_timeout=int(os.environ["INPUT_DEPLOYMENTS_TIMEOUT"]),
+        accepted_responses=[
+            int(x.strip()) for x in os.environ["INPUT_ACCEPTED_RESPONSES"].split(",")
+        ],
     )
 
-    reviewapp_build_data = _get_build_data(github_deployment_status_url, interval_arg)
+    # Delay the checks till the app is built
+    logger.info(f"Build time delay: {args.build_time_delay} seconds")
+    time.sleep(args.build_time_delay)
 
-    if reviewapp_build_data["state"] != SUCCESS:
-        raise ValueError(f"Review App Build state: {reviewapp_build_data['state']}")
+    logger.info(f"Statuses being accepted: {args.accepted_responses}")
 
-    review_app_url = f"https://{reviewapp_build_data['environment']}.herokuapp.com"
+    with open(os.environ["GITHUB_EVENT_PATH"]) as f:
+        pull_request_data = json.load(f)
 
-    _check_review_app_deployment_status(review_app_url, accepted_responses)
+    # Fetch the GitHub status URL
+    github_deployment_status_url = _get_github_deployment_status_url(
+        deployments_url=pull_request_data["repository"]["deployments_url"],
+        commit_sha=pull_request_data["pull_request"]["head"]["sha"],
+        timeout=args.deployments_timeout,
+        interval=args.interval,
+    )
+
+    # Fetch other build data
+    reviewapp_build_data = _get_build_data(
+        url=github_deployment_status_url, interval=args.interval
+    )
+
+    # Perform all the checks now
+
+    if Checks.build in args.checks:
+        # Check if the build was success
+        build_state = reviewapp_build_data["state"]
+        if build_state != BuildStates.success.value:
+            raise ValueError(f"Review App Build state: {build_state}")
+        
+    if Checks.response in args.checks:
+        # Delay the checks till the app is loads
+        logger.info(f"Load time delay: {args.load_time_delay} seconds")
+        time.sleep(args.load_time_delay)
+        
+        # Check the HTTP response from app URL
+        review_app_url = f"https://{reviewapp_build_data['environment']}.herokuapp.com"
+        _check_review_app_deployment_status(
+            review_app_url=review_app_url, accepted_responses=args.accepted_responses
+        )
 
     print("Successful")
 
